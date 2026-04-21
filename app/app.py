8f7fe004c7d08593aae970fbbbfb8159c0d2d231
Lashmi Adame <lashmiadameleyv@gmail.com>
2026-04-21 08:17:20 -0600

    holi

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hola desde Flask en Docker y Jenkins!, Antes de que mi partner haga commit"})
+    return jsonify({"message": "Hola desde Flask en Docker y Jenkins!, Antes de que mi partner haga commit, Main de Lashmi"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
